ec560a13d340d7cbf49067ff1d97cd899585542c
StelianLuparu <stelian_luparu@yahoo.com>
2021-02-07 14:31:26 +0200

    modified appointments app

--- appointment_tracker/appointments/views.py
+++ appointment_tracker/appointments/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+def appointment(request):
+    return HttpResponse("Hello, world. You're at the appointment's home page.")
